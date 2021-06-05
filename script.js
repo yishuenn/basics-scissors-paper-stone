ec4fe772a5c0669e92330f2d3088ec82ec30c464
jeremylim91 <jeremylim_91@live.com>
2021-06-06 00:05:00 +0800

    converted reversed SPS into a gamde mode

--- script.js
+++ script.js
@@ -5,17 +5,33 @@
 // if at all. If we used the variable SCISSORS everywhere and misspelled
 // it in 1 place, ESLint would help us detect that error before runtime.
 // String constant variable names are usually all uppercase by convention.
+// ---objects---
 var SCISSORS = 'scissors';
 var PAPER = 'paper';
 var STONE = 'stone';
 var REVERSED_SCISSORS = 'reversed scissors';
 var REVERSED_PAPER = 'reversed paper';
 var REVERSED_STONE = 'reversed stone';
+// ---Possible modes---
+var REGULAR = 'regular';
+var REVERSED_SPS = 'reversed sps';
+var KOREAN_SPS = 'korean sps';
+// ---Misc---
+var MODE_SELECTION_INSTRUCTIONS =
+  'Please enter one of the following to choose a game mode:<br>1. ' +
+  REGULAR +
+  '<br>2. ' +
+  REVERSED_SPS +
+  '<br>3. ' +
+  KOREAN_SPS;
 var REPLAY_INSTRUCTIONS =
   'Now you can type "scissors" "paper" or "stone" to play another round!';
 
 // Keep track of user's name to personalise the game.
 var userName = '';
+
+// Keep track of the current game mode ().
+var mode = '';
 
 // Initialise the number of player wins, computer wins, and draws to 0.
 var numPlayerWins = 0;
@@ -45,20 +61,24 @@ var getRandomObject = function () {
  * @param {string} computerObject
  */
 var doesPlayerBeatComputer = function (playerObject, computerObject) {
-  return (
+  if (mode == REGULAR || mode == KOREAN_SPS) {
     (playerObject == SCISSORS && computerObject == PAPER) ||
-    (playerObject == PAPER && computerObject == STONE) ||
-    (playerObject == STONE && computerObject == SCISSORS) ||
-    (playerObject == REVERSED_SCISSORS && computerObject == STONE) ||
-    (playerObject == REVERSED_PAPER && computerObject == SCISSORS) ||
-    (playerObject == REVERSED_STONE && computerObject == PAPER)
-  );
+      (playerObject == PAPER && computerObject == STONE) ||
+      (playerObject == STONE && computerObject == SCISSORS);
+  }
+  if (mode == REVERSED_SPS) {
+    return (
+      (playerObject == REVERSED_SCISSORS && computerObject == STONE) ||
+      (playerObject == REVERSED_PAPER && computerObject == SCISSORS) ||
+      (playerObject == REVERSED_STONE && computerObject == PAPER)
+    );
+  }
 };
 
 // Set a fn that will return an icon based on a  given object
 var getObjectIcon = function (object) {
   if (object == SCISSORS || object == REVERSED_SCISSORS) return ' ✂️';
-  if (object == PAPER || object == REVERSED_PAPER) return ' 📄';
+  if (object == PAPER || object == REVERSED_PAPER) return ' 🗒';
   if (object == STONE || object == REVERSED_STONE) return ' 🪨';
 };
 
@@ -98,13 +118,99 @@ var getDefaultWinLossMessage = function () {
   );
 };
 
-// Check whether player draws with computer
+/**
+ * Check whether player draws with computer objects
+ * @param {*} playerObject
+ * @param {*} computerObject
+ */
 var doesPlayerDrawWComputer = function (playerObject, computerObject) {
+  if (mode == REGULAR || mode == KOREAN_SPS) {
+    return playerObject == computerObject;
+  }
+  if (mode == REVERSED_SPS) {
+    return (
+      (playerObject == REVERSED_SCISSORS && computerObject == SCISSORS) ||
+      (playerObject == REVERSED_PAPER && computerObject == PAPER) ||
+      (playerObject == REVERSED_STONE && computerObject == STONE)
+    );
+  }
+};
+
+/**
+ * Check whether player's inputs are valid
+ * @param {*} userInput
+ */
+var validateInput = function (userInput) {
+  // validate the input according to the SPS game
+  if (mode == REGULAR || mode == KOREAN_SPS) {
+    return userInput == SCISSORS || userInput == PAPER || userInput == STONE;
+  }
+  // validate the input according to reversed SPS game
+  if (mode == REVERSED_SPS) {
+    return (
+      userInput == REVERSED_SCISSORS ||
+      userInput == REVERSED_PAPER ||
+      userInput == REVERSED_STONE
+    );
+  }
+  // return false to show that input is invalid
+  return false;
+};
+
+/**
+ * Create a string message saying that the user's  input is invalid
+ */
+var createResponseForInvalidInput = function () {
+  if (mode == REGULAR || mode == KOREAN_SPS) {
+    return (
+      'Please input 1 of ' +
+      SCISSORS +
+      ', ' +
+      PAPER +
+      ', or ' +
+      STONE +
+      ' to play the game.'
+    );
+  }
+  if (mode == REVERSED_STONE) {
+    return (
+      'Please input 1 of ' +
+      REVERSED_SCISSORS +
+      ', ' +
+      REVERSED_PAPER +
+      ', or ' +
+      REVERSED_STONE +
+      ' to play the game.'
+    );
+  }
+};
+
+var createPlayingInstructions = function () {
+  // if it is the reversed mode, prompt user to enter 'reversed scissors', 'reversed paper', ...
+  if (mode == REVERSED_SPS) {
+    return (
+      'Thank you for playing, ' +
+      userName +
+      '! <br> To begin, enter "' +
+      REVERSED_SCISSORS +
+      '" "' +
+      REVERSED_PAPER +
+      '" or "' +
+      REVERSED_STONE +
+      '".'
+    );
+  }
+  // if it is not the reversed mode, propmpt user to enter 'scissors', 'paper', or 'stone'
   return (
-    playerObject == computerObject ||
-    (playerObject == REVERSED_SCISSORS && computerObject == SCISSORS) ||
-    (playerObject == REVERSED_PAPER && computerObject == PAPER) ||
-    (playerObject == REVERSED_STONE && computerObject == STONE)
+    'Thank you for playing, ' +
+    userName +
+    '! <br> To begin, enter "' +
+    SCISSORS +
+    '" "' +
+    PAPER +
+    '" or "' +
+    STONE +
+    '".'
   );
 };
 
@@ -123,27 +229,35 @@ var main = function (input) {
     }
     userName = input;
 
-    return (
-      'Thank you ' +
-      userName +
-      '!  To start playing SPS, please enter "scissors", "paper", or "stone".'
-    );
+    return 'Welcome, ' + userName + '!<br>' + MODE_SELECTION_INSTRUCTIONS;
+  }
+  // If game mode is not yet set, accept user's second input the mode, and
+  // return an output that prompts the user to start playing SPS.
+
+  console.log(`mode is:`);
+  console.log(mode);
+  if (!mode) {
+    // If the user did not input anything, prompt them to enter a mode
+    if (!input) {
+      return MODE_SELECTION_INSTRUCTIONS;
+    }
+    console.log(`input is:`);
+    console.log(input);
+    mode = input;
+
+    // display the instructions to the user depending on the chosen mode
+    var playingInstructions = createPlayingInstructions();
+    return playingInstructions;
   }
 
-  // If userName is populated, validate that input is one of scissors, paper, or stone
-  if (
-    input != SCISSORS &&
-    input != PAPER &&
-    input != STONE &&
-    input != REVERSED_SCISSORS &&
-    input != REVERSED_PAPER &&
-    input != REVERSED_STONE
-  ) {
-    return 'Please input 1 of "scissors", "paper", or "stone" to play the game.';
+  // If userName and mode are populated, validate the input. What constitutes as valid will depend on what mode the user selected
+  var inputIsValid = validateInput(input);
+
+  if (inputIsValid == false) {
+    return createResponseForInvalidInput();
   }
 
-  // Input is one of scissors, paper, or stone
-  // Call input playerObject to make game logic clearer
+  // If input is valid, call it "playerObject" to make game logic clearer
   var playerObject = input;
   // Get computer's computer-generated object
   var computerObject = getRandomObject();
@@ -152,6 +266,7 @@ var main = function (input) {
     playerObject,
     computerObject
   );
+
   // Compare player's object with computer's object and output win status
   // If player and computer objects are the same, it's a draw.
   if (doesPlayerDrawWComputer(playerObject, computerObject)) {
