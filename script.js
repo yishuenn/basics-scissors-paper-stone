43c0bfc70a60fe4b44916909b8c770e7bf575ec4
awongh <akira@awongh.com>
2021-07-16 16:05:23 +0800

    add errors to test cc

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 var main = function (input) {
+
+
+    // make an unindented comment
   var myOutputValue = 'hello world';
+    "indentation assignment error";
   return myOutputValue;
 };
+
+var unUsedVariable = 999999;
