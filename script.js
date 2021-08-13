f77640ac59c813ed870fe3ab38e63cd580fffdb7
yishuenn <janicepeh97@gmail.com>
2021-08-14 00:11:17 +0800

    try2

--- script.js
+++ script.js
@@ -1,80 +1,60 @@
-//Default state
-var currentGameMode = 'waiting for player'
-var username = ''
+//Point system
+playerwin = 0
+playerloss = 0
+playerdraw = 0
 
 //Choices by user
 var rock = 'rock';
 var scissors = 'scissors';
 var paper = 'paper';
 
-//Output from player's input
-var outputfromplayer = function (input) {
-  var playerinput = 'error'
-  if (input == 'rock') {
-    playerinput = rock;
-  }
-  if (input = 'scissors') {
-    playerinput = scissors;
-  }
-  if (input = 'paper') {
-    playerinput = paper;
-  }
-  return playerinput;
-};
-
-// Computer Generating random number from 0-2
+///Fixing generation of 3 numbers & naming them to rock/scissors/paper
+// Computer Generating random numbers from 0-2
   var computerinput = function(){
     var randomDecimal = Math.random()*3;
     var randomInteger = Math.floor (randomDecimal);  
-   // Naming Number
-    if (randomInteger == 0) {
-      return rock;
-    }
-    if (randomInteger == 1) {
-      return scissors;
-    }
-    if (randomInteger ==2) {
-      return paper ;
-    };
-    myOutputValue = randomInteger
-  
-// Game rules: Player lose
-var playerlosewhen = function (playerinput, computerinput){
-  var playerlose = (playerinput == rock && computerinput == paper) || (playerinput == scissors && computerinput == rock) || (playerinput ==paper &&computerinput == scissors);
-  console.log (playerlose);
+    return randomInteger;
+  }
+// Naming Number to rock, scissors, papers
+ var numbertoword = function () {
+   var randomInteger = computerinput ();
+   if (randomInteger == 0) {return rock};
+   if (randomInteger == 1) {return scissors};
+    if (randomInteger ==2) {return paper};
+ }
 
-  return playerlose;}
+//Default state
+var currentGameMode = 'waiting for player'
+var username = ''
 
-//Game rules: Player wins
-var playerwinwhen = function (playerinput, computerinput){
-  var playerwin = (computerinput == rock && playerinput == paper) || (computerinput == scissors && playerinput == rock) || (computerinput ==paper && playerinput == scissors);
-  console.log (playerwin);
-
-  return playerwin;}
-
-// Game rules: Draw
-var playerdrawwhen = function (playerinput, computerinput)
-{var playerdraw = (computerinput == rock && playerinput == rock) || (computerinput == scissors && playerinput == scissors) || (computerinput ==paper && playerinput == paper);
-  console.log (playerdraw);
-
-  return playerdraw;}
-  
-  var main = function (input) {
+var main = function (input) {
   var myOutputValue = '';
 
   if (currentGameMode == 'waiting for player'){
-    //set username
-    username = input;
-    // Game starting
-    currentGameMode = 'Game started'
-    console.log = (currentGameMode)
-    myOutputValue = 'Hello' + username + 'Please choose rock, scissors or paper';
-  }
-
- // else if (currentGameMode == 'Gamestarted') {
-    // Playerinput
-    var playinput = outputfromplayer (input)
-    var computerinput = randomInteger(input)
-  }
+  // set username
+  input = username;
+  // Game mode been switched
+  currentGameMode = 'Game started';
+  myOutputValue = 'Hello' + username + 'Please enter rock/ scissors/ paper';
   return myOutputValue;
+  console.log(myOutputValue)};
+
+if (currentGameMode == 'Game started'){
+  // Draw situation
+  var computerchoice = numbertoword();
+  console.log(input, computerchoice);
+    if (input == computerchoice) {
+    playerdraw = playerdraw+1;
+    myOutputValue = 'You drawed'};
+
+  //Player wins
+    if (input ==scissors && computerchoice== paper) || (input == rock && computerchoice== scissors) || (input == paper && computerchoice == rock) {
+      playerwin = playerwin+1;
+      myOutputValue = 'You win'};
+
+  // Player lost
+    if (input ==paper && computerchoice== scissors) || (input == scissors && computerchoice== rock) || (input == rock && computerchoice == paper) {
+      playerlose = playerlose+1;
+      myOutputValue = 'You lost'};
 };
+  return myOutputValue;
